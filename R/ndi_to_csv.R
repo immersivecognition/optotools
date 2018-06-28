36ba09ef38ace2badeb08f577ac4e0242bcc4c8d
jackbrookes <jbrookes187@gmail.com>
2018-06-28 16:53:25 +0100

    fixed csv writing

--- R/ndi_to_csv.R
+++ R/ndi_to_csv.R
@@ -1,9 +1,14 @@
-#' Reads and converts an Optotrak file from given filepath and re-saves as a CSV file
+#' Reads and converts an Optotrak file from given filepath and re-saves as a CSV file.
+#' @param ndi_path string Location of the file to read.
+#' @param output_path string Location of the file to write.
+#' @param sep The field separator string. Values within each row of the dataframe are separated by this string.
+#' @param row.names Either a logical value indicating whether the row names of x are to be written along with x, or a character vector of row names to be written.
+#' @param ... Additional parameters passed to write.table
 #' @export
-ndi_to_csv <- function(ndi_path, output_path, sep = ",") {
+ndi_to_csv <- function(ndi_path, output_path, sep = ",", row.names = FALSE, ...) {
 
   opto_data <- read_ndi(ndi_path)
 
-  write.table(as.data.frame(opto_data), output_path, sep = sep)
+  write.table(as.data.frame(opto_data), output_path, sep = sep, row.names = row.names, ...)
 
 }
--- example.R
+++ example.R
@@ -5,7 +5,7 @@ opto_data <- read_ndi("test_data/C#001.ROO")
 
 # we can access properties such as frequency, number of markers, and more
 opto_data$freq
-opto_data$items
+opto_data$items 
 
 # to access the data, access the df object or convert using as.data.frame
 df <- as.data.frame(opto_data)
