5f4f22dfe5f90de1840f05a1123fbf45215c948b
jackbrookes <jbrookes187@gmail.com>
2018-06-28 16:42:59 +0100

    fixed sep

--- R/ndi_to_csv.R
+++ R/ndi_to_csv.R
@@ -4,6 +4,6 @@ ndi_to_csv <- function(ndi_path, output_path, sep = ",") {
 
   opto_data <- read_ndi(ndi_path)
 
-  write.csv(as.data.frame(opto_data), output_path, sep = sep)
+  write.table(as.data.frame(opto_data), output_path, sep = sep)
 
 }
